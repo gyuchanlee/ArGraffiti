0f7d93f3fd66f5dbdcde1f68b5ca03e937f4858c
gyuchanlee <hoo788@gmail.com>
2025-03-24 15:28:29 +0900

    first init

---
+++ src/test/java/com/dodo/argraffiti/ArGraffitiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dodo.argraffiti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArGraffitiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
